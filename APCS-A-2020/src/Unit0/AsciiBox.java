51482917d139ff274a6151a577561986bcb4938f
tiana1507 <tiana1507@CA-SU-F106-18.sduhsd.lan>
2020-01-31 10:27:03 -0800

    1/31/2020 Initial File Commit Upload

---
+++ APCS-A-2020/src/Unit0/AsciiBox.java
@@ -0,0 +1,33 @@
+package Unit0;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Andrew Tian \t  1/30/2020 \n\n" );
+		String str1 = "+++++++++++++++++++++++++";
+		String str2 = "AAAAAAAAAAAAAAAAAAAAAAAAA";
+		for (int i = 0; i <= 6; i++) {
+			//String Repeat works like this within Java 11, check if this is indeed Java 11, if not just use the more classic method
+			if (i % 2 == 0) {
+				System.out.println(str1);
+				System.out.println(str1);
+				System.out.println(str1);
+			}
+			else {
+				System.out.println(str2);
+				System.out.println(str2);
+			}
+			
+			
+		}
+
+	}
+}
