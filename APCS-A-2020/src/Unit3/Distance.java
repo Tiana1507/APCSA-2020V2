9407507ca78aeecdb59e9f636767fc7638f0d9d6
tiana1507 <tiana1507@CA-SU-F106-18.sduhsd.lan>
2020-02-05 11:53:05 -0800

    Ongoing attempt, 2/5/2020, inclass

--- APCS-A-2020/src/Unit3/Distance.java
+++ APCS-A-2020/src/Unit3/Distance.java
@@ -18,10 +18,22 @@ public class Distance
 
 	public Distance()
 	{
+		Scanner keyboard = new Scanner(System.in);
+
+		//ask for the input here
+		System.out.print("Enter X1 :: ");
+		int x1 = keyboard.nextInt();
+		System.out.print("Enter Y1 :: ");
+		int y1 = keyboard.nextInt();
+		System.out.print("Enter X2 :: ");
+		int x2 = keyboard.nextInt();
+		System.out.print("Enter Y2 :: ");
+		int y2 = keyboard.nextInt();
 	}
 
 	public Distance(int x1, int y1, int x2, int y2)
 	{
+		distance = Math.pow(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2), 2);
 	}
 
 	public void setCoordinates(int x1, int y1, int x2, int y2)
