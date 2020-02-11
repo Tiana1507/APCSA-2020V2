4fb133482cab50e4599e8980eb21dd69d5f6ab6d
tiana1507 <tiana1507@CA-SU-F106-18.sduhsd.lan>
2020-02-11 10:37:03 -0800

    Bug Fixes, 2/11/2020 InClass

--- APCS-A-2020/src/Unit5/FirstLastVowel.java
+++ APCS-A-2020/src/Unit5/FirstLastVowel.java
@@ -1,5 +1,13 @@
 package Unit5;
 
 public class FirstLastVowel {
+	//(c) A+ Computer Science
+	//www.apluscompsci.com
+	//Name -
+	//Date -
 
-}
+
+	   public static String go( String a ) {
+			return "add code here";
+		}
+	}
