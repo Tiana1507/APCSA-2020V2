649af71c69de32d99da19552842f65652c058b55
andre <andre@NewHope>
2020-02-17 15:28:26 -0800

    2/17/2020. Inhouse, compelted Perfect LAb

--- APCS-A-2020/src/Unit6/Perfect.java
+++ APCS-A-2020/src/Unit6/Perfect.java
@@ -9,14 +9,39 @@ public class Perfect
    private int number;
 
 	//add constructors
-
+   private boolean istrue;
 	//add a set method
-
+	public void setNum(int num)
+	{
+		number = num;
+	}
+   
 	public boolean isPerfect()
 	{
+		int divisor = 1;
+		int divisorsum = 0;
+		while (divisorsum < number) {
+			if (number % divisor == 0)
+				divisorsum += divisor;
+			divisor += 1;
+		}
+		if (divisorsum == number) {
+			istrue = true;
+			return true;
+		}
+		istrue = false;
 		return false;
+		
 	}
 
 	//add a toString	
-	
+	public String toString()
+	{
+		if (istrue == true) {
+			return number + " is perfect";
+		}
+		else {
+			return number + " is not perfect";
+		}
+	}
 }
--- APCS-A-2020/src/Unit6/PerfectRunner.java
+++ APCS-A-2020/src/Unit6/PerfectRunner.java
@@ -9,6 +9,48 @@ public class PerfectRunner
 	public static void main( String args[] )
 	{
 		//add test cases
-																
+		Perfect perfectchecker = new Perfect();
+		perfectchecker.setNum(496);
+		perfectchecker.isPerfect();
+		System.out.println(perfectchecker.toString());
+		perfectchecker.setNum(45);
+		perfectchecker.isPerfect();
+		System.out.println(perfectchecker.toString());
+		perfectchecker.setNum(6);
+		perfectchecker.isPerfect();
+		System.out.println(perfectchecker.toString());
+		perfectchecker.setNum(14);
+		perfectchecker.isPerfect();
+		System.out.println(perfectchecker.toString());
+		perfectchecker.setNum(8128);
+		perfectchecker.isPerfect();
+		System.out.println(perfectchecker.toString());
+		perfectchecker.setNum(1245);
+		perfectchecker.isPerfect();
+		System.out.println(perfectchecker.toString());
+		perfectchecker.setNum(33);
+		perfectchecker.isPerfect();
+		System.out.println(perfectchecker.toString());
+		perfectchecker.setNum(28);
+		perfectchecker.isPerfect();
+		System.out.println(perfectchecker.toString());
+		perfectchecker.setNum(27);
+		perfectchecker.isPerfect();
+		System.out.println(perfectchecker.toString());
+		perfectchecker.setNum(33550336);
+		perfectchecker.isPerfect();
+		System.out.println(perfectchecker.toString());
 	}
 }
+/*
+496
+45
+6
+14
+8128
+1245
+33
+28
+27
+33550336
+*/
