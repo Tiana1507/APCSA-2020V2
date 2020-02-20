9f571e610c3546d06a426bf8ecf0607d829d5979
tiana1507 <tiana1507@CA-SU-F106-18.sduhsd.lan>
2020-02-20 11:12:32 -0800

    codeattempt1

--- APCS-A-2020/src/Unit7/TriangleFive.java
+++ APCS-A-2020/src/Unit7/TriangleFive.java
@@ -20,14 +20,27 @@ public class TriangleFive
 
 	public void setLetter(char c)
 	{
+		c = letter;
 	}
 
 	public void setAmount(int amt)
 	{
+		amount = amt;
 	}
 
 	public String toString()
 	{
+		int cyclecounter = 0;
+		String alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		int theindex = alphabet.indexOf(letter);
+		while (amount > 0) {
+			for (int i = amount; i > 0; i--) {
+				System.out.print(alphabet.charAt(theindex));
+			}
+			System.out.print(" ");
+			amount -= 1;
+		}
+		
 		String output="";
 		return output;
 	}
