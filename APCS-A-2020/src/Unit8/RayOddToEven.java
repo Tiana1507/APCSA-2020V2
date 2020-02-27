9691ef01ee72553e5b624a5eb0f674d0397c5f76
andre <andre@NewHope>
2020-02-26 23:33:57 -0800

    updates

--- APCS-A-2020/src/Unit8/RayOddToEven.java
+++ APCS-A-2020/src/Unit8/RayOddToEven.java
@@ -1,13 +1,24 @@
 package Unit8;
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - andrew tian
 //Date -
 
+import static java.lang.System.*;
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public static int go(int[] ray)
 	{
-		return false;
-	}
-}
+		for(int i = 0; i <= ray.length-1; i++) {
+			if (ray[i]%2 == 1) {
+				for (int j = i; j <= ray.length-1; j++) {
+					if (ray[j]%2 == 0) {
+						return j-i;
+					}//end if
+				}//end for
+				return -1;
+			}//end if
+		}//end for
+		return -1;
+	}//end method
+}//end class
