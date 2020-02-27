53d7f75ed5ce9941660410735c2e7d3d9674070d
andre <andre@NewHope>
2020-02-26 23:37:59 -0800

    final update

--- APCS-A-2020/src/Unit8/RaySumLast.java
+++ APCS-A-2020/src/Unit8/RaySumLast.java
@@ -1,13 +1,25 @@
 package Unit8;
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - andrew tian
 //Date -
+
+import static java.lang.System.*;
 
 public class RaySumLast
 {
 	public static int go(int[] ray)
 	{
-		return 0;
+		int copy = ray[ray.length-1];
+		int sum = 0;
+		for (int i = 0; i <= ray.length-1; i++) {
+			if (ray[i] > copy) {
+				sum += ray[i];
+			}
+		}
+		if (sum == 0) {
+			return -1;
+		}
+		return sum;
 	}
 }
