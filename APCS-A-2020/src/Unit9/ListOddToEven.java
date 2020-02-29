211af25b4ad8f308ae267f3b1e01a36b10f552ad
andre <andre@NewHope>
2020-02-28 22:36:40 -0800

    final commit

--- APCS-A-2020/src/Unit9/ListOddToEven.java
+++ APCS-A-2020/src/Unit9/ListOddToEven.java
@@ -9,8 +9,28 @@ import java.util.List;
 
 public class ListOddToEven
 {
-	public static boolean go( List<Integer> ray )
+	//for some reason this code started out with a go function that demanded that a boolean value was returned?
+	//But the A+ guide asks to return integer, so I changed it to a public static int instead of public static boolean.
+	public static int go( List<Integer> ray )
 	{
-		return false;
+		List <Integer> nums = ray;
+		int returndist = 0;
+		boolean startcheck = false;
+	
+		for (int i = 0; i < nums.size(); i++){
+			if(nums.get(i) % 2 == 1 && startcheck == false) {
+				startcheck = true;
+				returndist = i * -1;
+				break;
+			}
+		}
+		for (int i = nums.size()-1; i > 0; i--){
+			if(nums.get(i) % 2 == 0 && startcheck == true) {
+				returndist = i + returndist;
+				return returndist;
+			}
+			
+		}
+		return -1;
 	}
 }
--- APCS-A-2020/src/Unit9/SumFirstRunner.java
+++ APCS-A-2020/src/Unit9/SumFirstRunner.java
@@ -32,22 +32,22 @@ public class SumFirstRunner
 		ArrayList<Integer> nums7;
 		nums7= new ArrayList<Integer>(Arrays.asList(10,10,10,11,456));
 		System.out.println(ListSumFirst.go(nums7));
-		ArrayList<Integer> nums9;
-		nums9= new ArrayList<Integer>(Arrays.asList(-111,1,2,3,9,11,20,1));
-		System.out.println(ListSumFirst.go(nums9));
-		ArrayList<Integer> nums12;
-		nums12= new ArrayList<Integer>(Arrays.asList(9,8,7,6,5,4,3,2,0,-2,6));
-		System.out.println(ListSumFirst.go(nums12));
 		ArrayList<Integer> nums8;
-		nums8= new ArrayList<Integer>(Arrays.asList(12,15,18,21,23,1000));
+		nums8= new ArrayList<Integer>(Arrays.asList(-111,1,2,3,9,11,20,1));
 		System.out.println(ListSumFirst.go(nums8));
+		ArrayList<Integer> nums9;
+		nums9= new ArrayList<Integer>(Arrays.asList(9,8,7,6,5,4,3,2,0,-2,6));
+		System.out.println(ListSumFirst.go(nums9));
 		ArrayList<Integer> nums10;
-		nums10= new ArrayList<Integer>(Arrays.asList(250,19,17,15,13,11,10,9,6,3,2,1,0));
+		nums10= new ArrayList<Integer>(Arrays.asList(12,15,18,21,23,1000));
 		System.out.println(ListSumFirst.go(nums10));
 		ArrayList<Integer> nums11;
-		nums11= new ArrayList<Integer>(Arrays.asList(9,10,-8,10000,-5000,1000));
+		nums11= new ArrayList<Integer>(Arrays.asList(250,19,17,15,13,11,10,9,6,3,2,1,0));
 		System.out.println(ListSumFirst.go(nums11));
-		/* SAMPLE OUTPUT
+		ArrayList<Integer> nums12;
+		nums12= new ArrayList<Integer>(Arrays.asList(9,10,-8,10000,-5000,1000));
+		System.out.println(ListSumFirst.go(nums12));
+		/* SAMPLE OUTPUT SUCESS
 		 * 60
 -1
 230
