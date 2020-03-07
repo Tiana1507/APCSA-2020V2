936f56837e84eec5ce712e19ed81e5e09a414220
Tiana1507 <andrew.zi.tian@gmail.com>
2020-03-06 20:26:04 -0800

    RESYNC PLEASE

--- APCS-A-2020/src/Elevens/DeckRunner.java
+++ APCS-A-2020/src/Elevens/DeckRunner.java
@@ -5,6 +5,8 @@ package Elevens;
 
 import static java.lang.System.*;
 
+import java.lang.reflect.Array;
+
 public class DeckRunner
 {
 	public static void main( String args[] )
@@ -13,19 +15,30 @@ public class DeckRunner
 		Deck test = new Deck();
 		
 		System.out.println( "All cards in order." );
-		for( int j = 1; j <= 52; j++ )
+		for( int i = 1; i <= 52; i++ )
 		{
+//<<<<<<< HEAD
+			System.out.println( test.dealCard(i) );
+//=======
 			System.out.println( test.deal() );
+//>>>>>>> branch 'master' of https://github.com/Tiana1507/APCSA-2020V2.git
 		}
 		
-		//cal the shuffle method (do this after the deck part)
+		System.out.println("shuffling...");
+		//call the shuffle method (do this after the deck part)
 		test.shuffle();	
+		
+		//test bit delete later
+		//System.out.println("reach here");
 		
 		//print out shuffled deck
 		System.out.println( "\n\nAll cards after shuffling." );
-		for( int j = 1; j <= 52; j++ )
+		for( int i = 1; i <= 52; i++ )
 		{
-			System.out.println( test.deal() );
+//<<<<<<< HEAD
+			System.out.println( test.dealCard(i) );
+//=======
+//>>>>>>> branch 'master' of https://github.com/Tiana1507/APCSA-2020V2.git
 		}				
 	}
 }
