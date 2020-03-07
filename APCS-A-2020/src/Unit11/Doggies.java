476426b004fbb607403b0ee63dfd58175091ec3e
Tiana1507 <andrew.zi.tian@gmail.com>
2020-03-06 23:29:48 -0800

    ughhh

--- APCS-A-2020/src/Unit11/Doggies.java
+++ APCS-A-2020/src/Unit11/Doggies.java
@@ -1,7 +1,7 @@
 package Unit11;
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - andrew tian
 
 import java.util.Arrays;
 
@@ -11,24 +11,42 @@ public class Doggies
 
 	public Doggies(int size)
 	{
-		//point pups at a new arry of Dog
-		
+		//point pups at a new aarry of Dog
+		Dog[] pups = new Dog[size];
+		this.pups = pups;
 	}
 	
 	public void set(int spot, int age, String name)
 	{
+		pups[spot] = new Dog (age,name);
 		//put a new Dog in the array at spot 
 		//make sure spot is in bounds		
 	}
 
 	public String getNameOfOldest()
 	{
-		return null;
+		int oldest = 0;
+		int oldestindex = 0;
+		for (int i = 0; i < pups.length;i++) {
+			if (pups[i].getAge() > oldest) {
+				oldest = pups[i].getAge();
+				oldestindex = i;
+			}
+		}
+		return pups[oldestindex].getName();
 	}
 
 	public String getNameOfYoungest()
 	{
-		return null;
+		int youngest = 2^32-1;
+		int youngestindex = 0;
+		for (int i = 0; i < pups.length;i++) {
+			if (pups[i].getAge() < youngest) {
+				youngest = pups[i].getAge();
+				youngestindex = i;
+			}
+		}
+		return pups[youngestindex].getName();
 	}
 
 	public String toString()
