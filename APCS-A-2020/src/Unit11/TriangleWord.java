91114dc875c055e41ddfdb26c80a379658ef4543
Tiana1507 <andrew.zi.tian@gmail.com>
2020-03-07 00:00:42 -0800

    manual commit

--- APCS-A-2020/src/Unit11/TriangleWord.java
+++ APCS-A-2020/src/Unit11/TriangleWord.java
@@ -1,7 +1,7 @@
 package Unit11;
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - andrew tian
 
 import java.util.Scanner;
 import static java.lang.System.*;
@@ -12,5 +12,13 @@ public class TriangleWord
 		
 	public static void printTriangle(String word)
 	{
+		String temp = "";
+		for (int i1 = 0; i1 <= word.length(); i1++) {
+			temp = word.substring(0,i1) + "";
+			for (int i2 = 0; i2 < i1; i2++) {
+				System.out.print(temp);
+			}
+			System.out.print("\n");
+		}
 	}
 }
