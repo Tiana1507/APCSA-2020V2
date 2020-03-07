9966e9092eb33674210600d24ec3f10fd06c1d8f
Tiana1507 <andrew.zi.tian@gmail.com>
2020-03-07 01:07:32 -0800

    manual commit

--- APCS-A-2020/src/Unit11/RationalRunner.java
+++ APCS-A-2020/src/Unit11/RationalRunner.java
@@ -1,7 +1,7 @@
 package Unit11;
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - andrew tian
 
 import static java.lang.System.*;
 
@@ -9,8 +9,7 @@ public class RationalRunner
 {
 	public static void main( String args[] )
 	{
-		/*uncomment once Rational class is built
-		 *
+		//uncomment once Rational class is built
 		Rational test = new Rational();
 		out.println("test = " + test);
 
@@ -65,7 +64,7 @@ public class RationalRunner
 		rTwo.setRational(2,8);
 		out.println("\n\n1/6.equals(2/8) = "+rOne.equals(rTwo));				
 		rOne.add(rTwo);
-		out.println("\n\n1/6.add(2/8) = "+ rOne);		
-		*/	
+		out.println("\n\n1/6.add(2/8) = "+ rOne);			
 	}
+	//ALL TESTS CORRECT!
 }
