b2081b2baf513ef34aef62bf72201615e6dd55be
tiana1507 <tiana1507@CA-SU-F106-18.sduhsd.lan>
2020-03-11 11:49:24 -0700

    finished cereal properties as well as importing sinbad ext JAR.

---
+++ APCS-A-2020/src/DataLab/Cereal.java
@@ -0,0 +1,54 @@
+package DataLab;
+import static java.lang.System.*;
+
+public class Cereal {
+		
+	private int protein;
+	private int carbs;
+	private String name;
+
+	public Cereal()
+	{
+	}
+
+	public Cereal (String n, int c, int p)
+	{
+		setCarbs(c);
+		setProtein(p);
+		setName(n);
+	}
+		
+	public int getCarbs()
+	{
+		return carbs;
+	}
+		
+	public void setCarbs( int c )
+	{
+		carbs = c;
+	}
+	
+	public int getProtein()
+	{
+		return protein;
+	}
+	
+	public void setProtein( int p) {
+		protein = p;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+		
+	public void setName( String n )
+	{
+		name = n;
+	}
+	
+	public String toString()
+	{
+	   return "Cereal: " + name + ", Protein: " + protein + "carbs: " + carbs;
+	}
+}
