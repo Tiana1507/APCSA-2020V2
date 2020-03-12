9495aa31c0fc80fd3dcc91046493aad263c3ce46
andre <andre@DESKTOP-KM5QO9T>
2020-03-12 01:00:08 -0700

    test123

---
+++ APCS-A-2020/src/resyncchecker/plzresync.java
@@ -0,0 +1,9 @@
+package resyncchecker;
+
+public class plzresync {
+	int hi = 0;
+    public static void main(String[] args) {
+        System.out.println("This will be printed");
+    }
+    //lets see if it autists
+}
