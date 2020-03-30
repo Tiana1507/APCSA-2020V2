d4cbc41dd3bfaebdc46920e1755d7f464799c515
Tiana1507 <andrew.zi.tian@gmail.com>
2020-03-30 11:30:30 -0700

    manual update

--- APCS-A-2020/src/Unit13/NumberShifterRunner.java
+++ APCS-A-2020/src/Unit13/NumberShifterRunner.java
@@ -1,1 +1,24 @@
+package Unit13;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - andrew tian
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import static java.lang.System.*;
+
+public class NumberShifterRunner
+{
+	public static void main( String args[] ) throws IOException
+	{
+		//10 test cases
+		for (int i = 0; i < 10; i++) {
+			int[] runray = NumberShifter.makeLucky7Array(20);
+			System.out.println("Original Array: " + Arrays.toString(runray));
+			NumberShifter.shiftEm(runray);
+			System.out.println("Shifted Array: " + Arrays.toString(runray) + "\n");
+		}
+	}
+}
