c9f47f57e12cd3b70dea5562a08094f62bd26deb
Tiana1507 <andrew.zi.tian@gmail.com>
2020-03-30 11:39:58 -0700

    manual update

--- APCS-A-2020/src/Unit13/NumberSorter.java
+++ APCS-A-2020/src/Unit13/NumberSorter.java
@@ -1,1 +1,49 @@
+package Unit13;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - andrew tian
 
+import java.util.Arrays; 
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import static java.lang.System.*;
+
+
+//LAB GOAL:
+//Send in a number and get back an array that contains all of the digits from the
+//number in sorted order. 634152 would come back [1, 2, 3, 4, 5, 6]. You may not use Strings in this program.
+
+
+public class NumberSorter
+{
+	//instance variables and other methods not shown
+
+	private static int getNumDigits(int number)
+	{
+		int count = 0;
+		while (number > 0) {
+			count++;
+			
+			//NTS: int will automatically round down.
+			number = number / 10;
+		}
+
+		return count;
+	}
+
+	public static int[] getSortedDigitArray(int number)
+	{
+		int[] sorted = new int[getNumDigits(number)];
+		int spot = 0;
+		
+		//while loop to load it up.
+		while (number > 0) {
+			//++ words but not +1?
+			sorted[spot++] = number % 10;
+			number = number / 10;
+		}
+		Arrays.sort(sorted);
+		return sorted;
+	}
+}
