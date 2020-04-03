a36d9009c38a58a557e9d59a150886d3832b5aa9
Tiana1507 <andrew.zi.tian@gmail.com>
2020-04-03 13:08:20 -0700

    manual update

--- APCS-A-2020/src/Unit13/WordSortTwoRunner.java
+++ APCS-A-2020/src/Unit13/WordSortTwoRunner.java
@@ -7,23 +7,29 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 import static java.lang.System.*;
 
 public class WordSortTwoRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
+		//trying to find the path of a file because I can't read it.
+	    File testFile = new File("wordsorttwo.dat");
+	    String currentPath = testFile.getAbsolutePath();
+	    System.out.println("current path is: " + currentPath);
+		
+		
 		//finds the wordsorttwo.dat file.
-		Scanner file = new Scanner(new File("wordsorttwo.dat"));
-		int size = file.nextInt();
+		Scanner file = new Scanner(currentPath);
+		int size = 3;
 		file.nextLine();
 		for (int i = 0; i < size; i++) {
 			String sentence = file.nextLine();
 			System.out.println(sentence);
-			//instantiate a new WordSort
-			WordSortTwo ws2 = new WordSortTwo(sentence);
-			ws2.sort();
-			System.out.println(ws2);
+			WordSortTwo sorter = new WordSortTwo(sentence);
+			sorter.sort();
+			System.out.println(sorter);
 		}
 	}
 }
