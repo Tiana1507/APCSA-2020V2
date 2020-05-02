aa87310807537ea11bce575532625f9ce51e5bcd
Tiana1507 <andrew.zi.tian@gmail.com>
2020-05-02 04:36:09 -0700

    format fix

--- APCS-A-2020/src/Pong/Wall.java
+++ APCS-A-2020/src/Pong/Wall.java
@@ -5,33 +5,28 @@ public class Wall extends Block
 	//this one is here to replace the old stuff
 	private int width, height;
 	
-	public Wall()
-	{
+	public Wall() {
 		setPos(0, 0);
 		width = 800;
 		height = 600;
 	}
 	
-	public Wall(int wid, int hei)
-	{
+	public Wall(int wid, int heit) {
 		setPos(0, 0);
 		width = wid;
-		height = hei;
+		height = heit;
 	}
 	
-	public void setWidthHeight(int wid, int hei)
-	{
+	public void setWidthHeight(int wid, int heit) {
 		width = wid;
-		height = hei;
+		height = heit;
 	}
 	
-	public int getWidth()
-	{
+	public int getWidth() {
 		return width;
 	}
 	
-	public int getHeight()
-	{
+	public int getHeight() {
 		return height;
 	}
 }
