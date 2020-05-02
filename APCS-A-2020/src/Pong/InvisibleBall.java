91f1af05435ce2ca0c595a257eb48e62df395522
Tiana1507 <andrew.zi.tian@gmail.com>
2020-05-02 04:32:57 -0700

    woulda been room for a good pun

--- APCS-A-2020/src/Pong/InvisibleBall.java
+++ APCS-A-2020/src/Pong/InvisibleBall.java
@@ -7,64 +7,49 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
-class InvisibleBall extends SpeedUpBall
+class InvisibleBall extends Ball
 {
 
  //constructors
  public InvisibleBall() {
-	super();
+	 super();
  }
 
  public InvisibleBall(int x, int y) {
-	  super(x, y);
+	   super(x, y);
  }
 
- public InvisibleBall(int x, int y, int wid, int ht) {
-	  super(x, y, wid, ht);
+ public InvisibleBall(int x, int y, int wid, int heit) {
+	   super(x, y, wid, heit);
  }
 
- public InvisibleBall(int x, int y, int wid, int ht, int xSpd, int ySpd) {
-	  super(x, y, wid, ht, xSpd, ySpd);
+ public InvisibleBall(int x, int y, int wid, int heit, int xSpd, int ySpd) {
+	   super(x, y, wid, heit, xSpd, ySpd);
  }
 
- public InvisibleBall(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd) {
-	  super(x, y, wid, ht, col, xSpd, ySpd);
+ public InvisibleBall(int x, int y, int wid, int heit, Color col, int xSpd, int ySpd) {
+	   super(x, y, wid, heit, col, xSpd, ySpd);
  }
 
- public Color noColor() {
-	 //this is all black so invisible.
- 	int r = 0;
-	int g = 0;
-	int b = 0;
-	return new Color(r,g,b);
- }
- 
- public Color regColor()
- {
-	//this is white so visible.
- 	int r = 255;
-	int g = 255;
-	int b = 255;
-	return new Color(r,g,b);
+ public Color theColor() {
+	 	//this is all black so invisible.
+ 		if (Math.random() * 100 > 50) {
+ 		 	System.out.println("invisible");
+ 			return new Color(0,0,0);
+ 		}
+ 		else {
+ 		 	//this is white so visible.
+ 			System.out.println("visible");
+ 			return new Color(255,255,255);
+ 		}
  }
 
  public void moveAndDraw(Graphics window)
  {
-	 //uses a random number generator to decide if invisible or not.
-	 if (Math.random()*100 > 50 ) {
-		//regular white ball
-		 draw(window, Color.BLACK);
-		 setX(getX()+getXSpeed());
-		 setY(getY()+getYSpeed());
-		 draw(window, regColor());
-	 }
-	 else {
-		   //black is the BG colour
-		draw(window, Color.BLACK);
-		setX(getX()+getXSpeed());
-		setY(getY()+getYSpeed());
-		draw(window, noColor());
-	 }
-	 
- 	}
+	   draw(window, Color.BLACK);
+	   setX(getX()+getXSpeed());
+	   setY(getY()+getYSpeed());
+	   draw(window, theColor());
+
+ }
 }
