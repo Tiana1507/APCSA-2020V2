da39f501a98669409a98c907251333c25532ef3b
Tiana1507 <andrew.zi.tian@gmail.com>
2020-05-02 03:15:48 -0700

    wierd tabing

--- APCS-A-2020/src/Pong/InvisibleBall.java
+++ APCS-A-2020/src/Pong/InvisibleBall.java
@@ -12,59 +12,59 @@ class InvisibleBall extends SpeedUpBall
 
  //constructors
  public InvisibleBall() {
-	 super();
+	super();
  }
 
  public InvisibleBall(int x, int y) {
-	   super(x, y);
+	  super(x, y);
  }
 
  public InvisibleBall(int x, int y, int wid, int ht) {
-	   super(x, y, wid, ht);
+	  super(x, y, wid, ht);
  }
 
  public InvisibleBall(int x, int y, int wid, int ht, int xSpd, int ySpd) {
-	   super(x, y, wid, ht, xSpd, ySpd);
+	  super(x, y, wid, ht, xSpd, ySpd);
  }
 
  public InvisibleBall(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd) {
-	   super(x, y, wid, ht, col, xSpd, ySpd);
+	  super(x, y, wid, ht, col, xSpd, ySpd);
  }
 
  public Color noColor() {
-	 	//this is all black so invisible.
- 		int r = 0;
-		int g = 0;
-		int b = 0;
-		return new Color(r,g,b);
+	 //this is all black so invisible.
+ 	int r = 0;
+	int g = 0;
+	int b = 0;
+	return new Color(r,g,b);
  }
  
  public Color regColor()
  {
-	 	//this is white so visible.
- 		int r = 256;
-		int g = 256;
-		int b = 256;
-		return new Color(r,g,b);
+	//this is white so visible.
+ 	int r = 256;
+	int g = 256;
+	int b = 256;
+	return new Color(r,g,b);
  }
 
  public void moveAndDraw(Graphics window)
  {
 	 //uses a random number generator to decide if invisible or not.
 	 if (Math.random()*100 > 50 ) {
-		   //regular white ball
-		   draw(window, Color.BLACK);
-		   setX(getX()+getXSpeed());
-		   setY(getY()+getYSpeed());
-		   draw(window, regColor());
+		//regular white ball
+		 draw(window, Color.BLACK);
+		 setX(getX()+getXSpeed());
+		 setY(getY()+getYSpeed());
+		 draw(window, regColor());
 	 }
 	 else {
 		   //black is the BG colour
-		   draw(window, Color.BLACK);
-		   setX(getX()+getXSpeed());
-		   setY(getY()+getYSpeed());
-		   draw(window, noColor());
+		draw(window, Color.BLACK);
+		setX(getX()+getXSpeed());
+		setY(getY()+getYSpeed());
+		draw(window, noColor());
 	 }
 	 
- }
+ 	}
 }
