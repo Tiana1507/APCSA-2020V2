85683d302c34c264ff9e4e906263ac9ed142b934
Tiana1507 <andrew.zi.tian@gmail.com>
2020-05-08 22:41:51 -0700

    fancier snowflakes

--- APCS-A-2020/src/WinterScene/FancySnowflake.java
+++ APCS-A-2020/src/WinterScene/FancySnowflake.java
@@ -34,6 +34,10 @@ public class FancySnowFlake extends AbstractShape
 	public void draw(Graphics window)
 	{
 		window.fillOval(getXPos(), getYPos() , 5, 5);
+		window.fillOval(getXPos()+5, getYPos() , 5, 5);
+		window.fillOval(getXPos()-5, getYPos() , 5, 5);
+		window.fillOval(getXPos(), getYPos()+5 , 5, 5);
+		window.fillOval(getXPos(), getYPos()-5 , 5, 5);
 		//for some reason it wont iterate so Ill use a goddamn list then.
 		//int[] LISTVALUES = {40,60,80,100,120,140,160,180,200,220,240,260,280,300,320,340,360,380,400,420,440,460,480,500,520,540,560};
 		//for (int i = 0; i< LISTVALUES.length;i++) {
