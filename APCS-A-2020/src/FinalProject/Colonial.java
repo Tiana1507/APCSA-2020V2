e06202cc21602b95df6a9be8d9704a8dc0bcbebd
Tiana1507 <andrew.zi.tian@gmail.com>
2020-06-05 18:02:13 -0700

    Manual commit

---
+++ APCS-A-2020/src/FinalProject/Colonial.java
@@ -0,0 +1,15 @@
+package FinalProject;
+
+import java.util.ArrayList;
+
+public class Colonial extends Provincial{
+	public Colonial(int P) {
+		super(P);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArrayList<MP> ColonialLegislature= new ArrayList<MP>();
+	//pretty much the same as a municipality lol.
+	
+	//next update should add in nationalist component as well as independence.
+}
