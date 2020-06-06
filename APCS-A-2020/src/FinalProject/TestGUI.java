b0b21b0fa475f8ea58fa2b98bae746bc07af9510
Tiana1507 <andrew.zi.tian@gmail.com>
2020-06-05 17:41:38 -0700

    Updated

--- APCS-A-2020/src/FinalProject/TestGUI.java
+++ APCS-A-2020/src/FinalProject/TestGUI.java
@@ -198,7 +198,169 @@ public class TestGUI extends JFrame {
 	        
     }
     }
-    
+        /*
+    //MainPanel2 is for regular 
+    private class MainPanel2 extends JPanel { 
+        public MainPanel2() {
+            super();
+        }
+        
+	    //draw graphics
+	        @Override
+	    public void paintComponent(Graphics g) {
+		        super.paintComponent(g);
+		        
+		        //first create a white BG
+		        g.setColor(Color.WHITE);
+		        g.fillRect(0,0,WIDTH,HEIGHT);
+		        
+		        //create a new Municipality.
+		        Muni.setYear(1904);
+		        
+				//pullthedata from the ifle/
+		        Muni.dataUpdate();
+		        Muni.popularityUpdate();
+		        System.out.println("commies: " + Muni.getCommunist());
+		        Muni.popularityUpdate();
+		        System.out.println("commies: " + Muni.getCommunist());
+		        Muni.popularityUpdate();
+		        System.out.println("commies: " + Muni.getCommunist());
+		        Muni.popularityUpdate();
+		        System.out.println("commies: " + Muni.getCommunist());
+		        Muni.popularityUpdate();
+		        System.out.println("commies: " + Muni.getCommunist());
+		        
+		        //System.out.println("New: " + Muni.toString());
+		        //Muni.electionUpdate(0);
+		        //Muni.setSeats(10);
+		        
+		        //the bar graph will start at x = 20, y = 120. POPULARITY
+		        //bar graphs will be 20 wide.
+		        //they will be between 0 and 100 units tall.
+				//each one will be 10 apart. 
+				
+				
+				//second bargraph will be below - NUMBER OF SEATS
+				//start at x = 20, y = 240
+		        //they will be between 0 and 100 units tall.
+				//each one will be 10 apart. 
+		        
+				//Party 1
+				g.setColor(Color.RED);
+				//this is for their actual popularity. The Height of the bar will be determined by their Party's Popularity (percentage)
+				//remember its x, y, width, height
+				g.fillRect(20,520,30, - 1 *Muni.getPartyPercentage(Muni.getParties().get(0))); //Multiplied by -1 since its the reverse when drawed.
+		        System.out.println("Communist: " + Muni.getPartyPercentage(Muni.getParties().get(0)));
+		        
+				
+		        //Party 2
+				g.setColor(Color.ORANGE);
+				//this is for their actual popularity. The Height of the bar will be determined by their Party's Popularity (percentage)
+				//remember its x, y, width, height
+				g.fillRect(60,520,30,-1 * 5 * Muni.getPartyPercentage(Muni.getParties().get(1)));
+				
+		        //Party 3
+				g.setColor(Color.CYAN);
+				//this is for their actual popularity. The Height of the bar will be determined by their Party's Popularity (percentage)
+				//remember its x, y, width, height
+				g.fillRect(100,520,30,-1 * 5 *Muni.getPartyPercentage(Muni.getParties().get(2)));
+		        
+		        //Party 4
+				g.setColor(Color.PINK);
+				//this is for their actual popularity. The Height of the bar will be determined by their Party's Popularity (percentage)
+				//remember its x, y, width, height
+				g.fillRect(140,520,30,-1 * 5 *Muni.getPartyPercentage(Muni.getParties().get(3)));
+				
+		        //Party 5
+				g.setColor(Color.GREEN);
+				//this is for their actual popularity. The Height of the bar will be determined by their Party's Popularity (percentage)
+				//remember its x, y, width, height
+				g.fillRect(180,520,30,-1 * 5 *Muni.getPartyPercentage(Muni.getParties().get(4)));
+				
+		        //Party 6
+				g.setColor(Color.YELLOW);
+				//this is for their actual popularity. The Height of the bar will be determined by their Party's Popularity (percentage)
+				//remember its x, y, width, height
+				g.fillRect(220,520,30,-1 * 5 *Muni.getPartyPercentage(Muni.getParties().get(5)));
+				
+		        //Party 7
+				g.setColor(Color.BLUE);
+				//this is for their actual popularity. The Height of the bar will be determined by their Party's Popularity (percentage)
+				//remember its x, y, width, height
+				g.fillRect(260,520,30,-1 * 5 *Muni.getPartyPercentage(Muni.getParties().get(6)));
+				
+		        //Party 8
+				g.setColor(Color.MAGENTA);
+				//this is for their actual popularity. The Height of the bar will be determined by their Party's Popularity (percentage)
+				//remember its x, y, width, height
+				g.fillRect(300,520,30,-1 * 5 *Muni.getPartyPercentage(Muni.getParties().get(7)));
+				
+		        //Party 9
+				g.setColor(Color.LIGHT_GRAY);
+				//this is for their actual popularity. The Height of the bar will be determined by their Party's Popularity (percentage)
+				//remember its x, y, width, height
+				g.fillRect(340,520,30,-1 * 5 *Muni.getPartyPercentage(Muni.getParties().get(8)));
+				
+		        //Party 10
+				g.setColor(Color.BLACK);
+				//this is for their actual popularity. The Height of the bar will be determined by their Party's Popularity (percentage)
+				//remember its x, y, width, height
+				g.fillRect(380,520,30,-1 * 5 *Muni.getPartyPercentage(Muni.getParties().get(9)));
+				
+		        //Party 11
+				g.setColor(Color.DARK_GRAY);
+				//this is for their actual popularity. The Height of the bar will be determined by their Party's Popularity (percentage)
+				//remember its x, y, width, height
+				g.fillRect(420,520,30,-1 * 5 *Muni.getPartyPercentage(Muni.getParties().get(10)));
+		        
+				//g.drawString(Muni.toString(),540,100);
+		        
+				
+				//Party List
+				g.setColor(Color.BLACK);
+				g.setFont(new Font("TIMES NEW ROMAN",Font.CENTER_BASELINE,24));
+				g.drawString("Parties and their Popularity", 500, 250);
+				g.setFont(new Font("TIMES NEW ROMAN",Font.ITALIC,24));
+				g.setColor(Color.RED);
+				g.drawString("The People's Party (*) - ~" + Muni.getPartyPercentage(Muni.getParties().get(0))+"%", 500, 275);
+				g.setColor(Color.ORANGE);
+				g.drawString("The Worker's Federation (*) - ~" + Muni.getPartyPercentage(Muni.getParties().get(1)) + "%", 500, 300);
+				g.setColor(Color.CYAN);
+				g.drawString("The Popular Front (*) - ~"+ Muni.getPartyPercentage(Muni.getParties().get(2)) + "%", 500, 325);
+				g.setColor(Color.PINK);
+				g.drawString("Labour Party - ~"+ Muni.getPartyPercentage(Muni.getParties().get(3)) + "%",500, 350);
+				g.setColor(Color.GREEN);
+				g.drawString("Liberal Democratic Party - ~"+ Muni.getPartyPercentage(Muni.getParties().get(4)) + "%",500, 375);
+				g.setColor(Color.YELLOW);
+				g.drawString("Libertarian Party - ~"+ Muni.getPartyPercentage(Muni.getParties().get(5)) + "%",500,400);
+				g.setColor(Color.BLUE);
+				g.drawString("Constitutionalist Conservative Party - ~"+ Muni.getPartyPercentage(Muni.getParties().get(6)) + "%",500, 425);
+				g.setColor(Color.MAGENTA);
+				g.drawString("The Imperial Restoration Clique(*) - ~"+ Muni.getPartyPercentage(Muni.getParties().get(7)) + "%",500, 450);
+				g.setColor(Color.LIGHT_GRAY);
+				g.drawString("The National Front(*) - ~" + Muni.getPartyPercentage(Muni.getParties().get(8)) + "%",500, 475);
+				g.setColor(Color.BLACK);
+				g.drawString("The Spiritual Revival Association(*) - ~"+ Muni.getPartyPercentage(Muni.getParties().get(9)) + "%",500, 500);
+				g.setColor(Color.DARK_GRAY);
+				g.drawString("National Harmony Party(*) - ~"+ Muni.getPartyPercentage(Muni.getParties().get(10)) + "%",500, 525);
+				
+				g.setColor(Color.BLACK);
+				g.setFont(new Font("TIMES NEW ROMAN",Font.BOLD,64));
+				g.drawString("Year: " + Muni.getYear(),500,80);
+			try {
+				Muni.popularityUpdate();
+				Muni.writetofile();
+				System.out.println("Wrote into file!");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        }
+	        private void update (Municipal Muni) {
+	        	
+	        }
+    }
+    */
 
     public static void main (String [] args) {
         new TestGUI();
